5393498f99224f6c19f1af4bf907c18882f56a71
Jim Sangwine <jim@sangwine.net>
2020-08-05 15:29:39 +0800

    Fix layer 1 trigger

--- Leopold FC660C/qmk/keymap.c
+++ Leopold FC660C/qmk/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		LT(2,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
 		KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,
-		LT(3,KC_CAPS), KC_LALT, KC_LGUI, KC_SPC, TO(1), KC_RCTL, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+		LT(3,KC_CAPS), KC_LALT, KC_LGUI, KC_SPC, MO(1), KC_RCTL, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
 	),
 	/* Function keys and coding symbols.
 	* ,--------------------------------------------------------------------------------------------------.
