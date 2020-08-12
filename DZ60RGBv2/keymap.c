6ed716e72de0b72f42bad7a39d1738b5a3d15467
Jim Sangwine <jim@sangwine.net>
2020-08-12 10:58:29 +0800

    Update dz60rgbv2 layout

--- DZ60RGBv2/keymap.c
+++ DZ60RGBv2/keymap.c
@@ -8,11 +8,11 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEFAULT] = LAYOUT_60_ansi(
-        KC_GESC,             KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,        KC_MINS, KC_EQL,  KC_BSPC,
-        LT(_NAV,KC_TAB),     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,        KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_LCTL,             KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,     KC_QUOT,          KC_ENT,
-        KC_LSFT,             KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,     KC_RSFT,
-        LT(_NUMPAD,KC_CAPS), KC_LALT, KC_LGUI,                            KC_SPC,                             MO(_CODING), KC_RCTL, KC_RALT, MO(_RGB)
+        MT(MOD_LCTL | MOD_LGUI,KC_GESC), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,        KC_MINS, KC_EQL,  KC_BSPC,
+        MT(MOD_LCTL,KC_TAB),             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,        KC_LBRC, KC_RBRC, KC_BSLS,
+        MO(_NAV),                        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,     KC_QUOT,           KC_ENT,
+        KC_LSFT,                                  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,      KC_SLSH,          KC_RSFT,
+        LT(_NUMPAD,KC_CAPS),          KC_LALT, KC_LGUI,                            KC_SPC,                                MO(_CODING), KC_RCTL, KC_RALT, MO(_RGB)
     ),
     [_CODING] = LAYOUT_60_ansi(
          KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL,
@@ -22,11 +22,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                            _______, _______, _______, _______
     ),
     [_NAV] = LAYOUT_60_ansi(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, KC_HOME,   KC_UP,  KC_END, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, KC_LEFT, _______, KC_RGHT, _______, KC_TILD,          _______,
-        _______,          _______, _______, _______, _______, _______, KC_PGDN, KC_DOWN, KC_PGUP, _______, _______,          _______,
-        _______, _______, _______,                            _______,                            _______, _______, _______, _______
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
+        _______, _______, _______, _______, _______, _______, _______, KC_HOME,   KC_UP,  KC_END, _______, KC_TILD, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_LEFT, _______, KC_RGHT, _______,  KC_GRV,          _______,
+        _______,          _______, _______, _______, _______, _______, KC_PGDN, KC_DOWN, KC_PGUP, _______, _______,            KC_UP,
+        _______, _______, _______,                            _______,                            _______, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_NUMPAD] = LAYOUT_60_ansi(
           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_NO,   KC_NO, KC_BSPC,
