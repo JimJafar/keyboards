e38644cd501b230c6f686337ee7bce5c1be3c5c3
Jim Sangwine <jim@sangwine.net>
2021-06-03 09:48:56 +0800

    Update dz60 split spacebar layout

--- DZ60_split_spacebar/commit.sh
+++ DZ60_split_spacebar/commit.sh
@@ -8,6 +8,8 @@ git commit . -m"Update dz60 split spacebar layout"
 git push
 
 echo "Pulling in upstream changes"
+
+cd ~/qmk_firmware
 git checkout master
 git fetch upstream
 git pull upstream master
@@ -19,6 +21,7 @@ git push
 
 echo "Copying changes to NextCloud"
 
+cd ~/qmk_firmware/keyboards/dz60/keymaps/EvilJimJafar
 cp config.h keymap.c ~/Nextcloud/Documents/keyboards/DZ60_split_spacebar/
 
 echo "Committing and pushing changes to EvilJimJafar/keyboards"
