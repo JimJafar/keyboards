9552936a6b4c03df67bee881ccec825ebbd4433b
Jim Sangwine <jim@sangwine.net>
2022-10-31 22:33:30 +0000

    Update YMDK NumPad layout

--- YMDK_NumPad/commit.sh
+++ YMDK_NumPad/commit.sh
@@ -2,14 +2,14 @@
 
 echo "Committing and pushing changes to EvilJimJafar/qmk_firmware"
 
-cd ~/qmk_firmware/keyboards/ymdk/np24/u4rgb6/keymaps/EvilJimJafar
+cd ~/code/qmk_firmware/keyboards/ymdk/np24/u4rgb6/keymaps/EvilJimJafar
 git checkout eviljimjafar
 git commit . -m"Update YMDK NumPad layout"
 git push
 
 echo "Pulling in upstream changes"
 
-cd ~/qmk_firmware
+cd ~/code/qmk_firmware
 git checkout master
 git fetch upstream
 git pull upstream master
@@ -27,7 +27,7 @@ echo "Copying changes to NextCloud"
 
 cp ymdk_np24_u4rgb6_EvilJimJafar.hex ~/Nextcloud/Documents/keyboards/YMDK_NumPad/
 
-cd ~/qmk_firmware/keyboards/ymdk/np24/u4rgb6/keymaps/EvilJimJafar
+cd keyboards/ymdk/np24/u4rgb6/keymaps/EvilJimJafar
 cp keymap.c ~/Nextcloud/Documents/keyboards/YMDK_NumPad/
 
 echo "Committing and pushing changes to EvilJimJafar/keyboards"
