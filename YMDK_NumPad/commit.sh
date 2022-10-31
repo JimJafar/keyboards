ec5a8d70152218c908891f5627e2c2d7af926ce4
Jim Sangwine <jim@sangwine.net>
2022-10-31 22:26:13 +0000

    Update YMDK NumPad layout

--- YMDK_NumPad/commit.sh
+++ YMDK_NumPad/commit.sh
@@ -19,7 +19,13 @@ git pull origin master
 git commit
 git push
 
+echo "Compiling firmware"
+
+qmk compile -kb ymdk/np24/u4rgb6 -km EvilJimJafar
+
 echo "Copying changes to NextCloud"
+
+cp ymdk_np24_u4rgb6_EvilJimJafar.hex ~/Nextcloud/Documents/keyboards/YMDK_NumPad/
 
 cd ~/qmk_firmware/keyboards/ymdk/np24/u4rgb6/keymaps/EvilJimJafar
 cp keymap.c ~/Nextcloud/Documents/keyboards/YMDK_NumPad/
@@ -27,6 +33,7 @@ cp keymap.c ~/Nextcloud/Documents/keyboards/YMDK_NumPad/
 echo "Committing and pushing changes to EvilJimJafar/keyboards"
 
 cd ~/Nextcloud/Documents/keyboards/YMDK_NumPad/
+git add --all
 git commit . -m"Update YMDK NumPad layout"
 git push
 
