c072ffb9d5ba8d67ca4e0bef20a36a969eeb8ffb
Jim Sangwine <jim@sangwine.net>
2024-09-14 13:00:30 +0100

    Update kyria split kb layout

---
+++ kyria/flash.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd ~/code/qmk_firmware/keyboards/splitkb/kyria/keymaps/EvilJimJafar
+
+# Compile
+qmk compile -kb kyria -km EvilJimJafar
+
+# Flash (put keyboard in bootloader mode first.)
+qmk flash -kb kyria -km EvilJimJafar
