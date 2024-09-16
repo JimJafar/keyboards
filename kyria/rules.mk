1cf9b9e1d8aa3d29c403f2a95a6debaf2dce9ae4
Jim Sangwine <jim@sangwine.net>
2024-09-16 14:49:49 +0100

    Update kyria split kb layout

--- kyria/rules.mk
+++ kyria/rules.mk
@@ -1,4 +1,1 @@
-OLED_ENABLE = yes
-OLED_DRIVER = ssd1306   # Enables the use of OLED displays
-ENCODER_ENABLE = no       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+DEFAULT_FOLDER = splitkb/kyria/rev3
