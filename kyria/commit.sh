8bfe00b0e62b98f18328b8bc05e1b69ec6a1fc03
Jim Sangwine <jim@sangwine.net>
2024-09-16 12:15:39 +0100

    Update kyria layout

--- kyria/commit.sh
+++ kyria/commit.sh
@@ -18,16 +18,17 @@ git checkout eviljimjafar
 git pull origin master
 git commit
 git push
+git checkout eviljimjafar
 
 echo "Compiling firmware"
 
+#cd ~/code/qmk_firmware/keyboards/splitkb/kyria/keymaps/EvilJimJafar
 qmk compile -kb splitkb/kyria/rev2 -km EvilJimJafar
 
 echo "Copying changes to Documents for syncing to NAS"
 
 cp splitkb_kyria_rev2_EvilJimJafar.hex ~/Documents/keyboards/kyria/
 
-cd keyboards/splitkb/kyria/keymaps/EvilJimJafar
 cp config.h keymap.c rules.mk ~/Documents/keyboards/kyria/
 
 echo "Committing and pushing changes to EvilJimJafar/keyboards"
